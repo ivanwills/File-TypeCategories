9ad4bba7e893526b11747fb203cef543cff10432
Ivan Wills <ivan.wills@gmail.com>
2015-01-07 08:09:12 +1100

    Updating for release of version 0.02

--- lib/File/TypeCategories.pm
+++ lib/File/TypeCategories.pm
@@ -17,7 +17,7 @@ use Types::Standard -types;
 use File::ShareDir qw/dist_dir/;
 use YAML qw/LoadFile/;
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 our %warned_once;
 
 has ignore => (
@@ -202,7 +202,7 @@ File::TypeCategories - Determine if files match a specific type
 
 =head1 VERSION
 
-This documentation refers to File::TypeCategories version 0.01
+This documentation refers to File::TypeCategories version 0.02
 
 =head1 SYNOPSIS
 
